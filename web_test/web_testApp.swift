3751264d09bf688354fe0293c4cdc2e65b5fa6c0
HaikDivanyan <70823905+HaikDivanyan@users.noreply.github.com>
2021-02-20 21:38:12 -0800

    Initial Commit

---
+++ web_test/web_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  web_testApp.swift
+//  web_test
+//
+//  Created by Haik Divanyan on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct web_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
